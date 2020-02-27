4e0f2d7442d37398c524267a2bbf2e11824a230e
sayandip <sayandip199309@gmail.com>
2020-02-27 17:01:46 +0530

    Add new disk_usage.py

---
+++ disk_usage.py
@@ -0,0 +1,21 @@
+import shutil
+import sys
+
+def check_disk_usage(disk, min_absolute, min_percent):
+    """Returns True it there us enough free disk space, False otherwise."""
+    du = shutil.disk_usage(disk)
+    # Calculate percentage of free disk space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    print(f"Available disk space: {gigabytes_free:.2f} GB ({percent_free:.2f}%)")
+    if percent_free < min_percent or gigabytes_free < min_absolute:
+        return False
+    return True
+
+if not check_disk_usage("/", 2, 10):
+    print("ERROR: Not enough disk space")
+    sys.exit(0)
+    
+print("Everything OK")
+sys.exit(1)
