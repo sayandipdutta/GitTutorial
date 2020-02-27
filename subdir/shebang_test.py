da26a87581bbe64c5ecd9bfc5e47adc5ba2efd2c
sayandip <sayandip199309@gmail.com>
2020-02-27 17:47:10 +0530

    Created example_commit.txt, created "subdir" directory
    Created example_commit.txt with helpful commit instructions.
    Moved shebang.py to newly created "subdir" directory.

---
+++ subdir/shebang_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import os
+
+def check_reboot():
+    """Returns True if the computer is pending a reboot."""
+    return os.path.exist("/run/reboot-required")
+
+def main():
+    pass
+
+main()
